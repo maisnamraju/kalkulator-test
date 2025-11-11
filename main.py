ca0c4d3d5e09222d4aa8dbe79d214cd844039fa5
Raju Singh <maisnamraju@gmail.com>
2025-11-11 09:12:29 +0700

    simple explaination

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 from typing import Union
 
 from fastapi import FastAPI
+from fastapi import Form
 from fastapi.requests import Request
 from fastapi.responses import HTMLResponse
 from fastapi.templating import Jinja2Templates
@@ -19,3 +20,12 @@ def read_root(
     request: Request,
 ):
     return templates.TemplateResponse("test2.html", {"request": request})
+
+@app.post("/submit")
+def submit(
+    request: Request,
+    num1: int = Form(...),
+    num2: int = Form(...),
+):
+    result = num1 + num2
+    return templates.TemplateResponse("test2.html", {"request": request, "result": result})
